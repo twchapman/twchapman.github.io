927f4adbf83f85ff54f90a9c40a340cba0f45bea
Travis Chapman <traviswchapman@gmail.com>
2020-04-07 19:47:52 -0400

    Gatsby-based blog forked from @wangonya

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  siteMetadata: {
+    title: `Travis Chapman`,
+    description: `I like finding elegant solutions to complex problems. But I like video games more.`,
+    author: `Travis Chapman`,
+    siteUrl: `https://twchapman.com`,
+    social: {
+      twitter: `twchapman`,
+      facebook: ``,
+      github: `twchapman`,
+      email: `mail@twchapman.com`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 970,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `flexible-gatsby-starter`,
+        short_name: `flexible-gatsby`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `./static/saturn.png`, // This path is relative to the root of the site.
+      },
+    },
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
+}
